d08d456925c9bb0944fdd6785e0d7797de1d24b1
Njagi Mwaniki <mwanikibusiness@gmail.com>
2015-02-17 04:51:23 +0300

    Added handlers PayslipNew and PayslipShow. Removed POST from HomeR

---
+++ Handler/PayslipNew.hs
@@ -0,0 +1,9 @@
+module Handler.PayslipNew where
+
+import Import
+
+getPayslipNewR :: Handler Html
+getPayslipNewR = error "Not yet implemented: getPayslipNewR"
+
+postPayslipNewR :: Handler Html
+postPayslipNewR = error "Not yet implemented: postPayslipNewR"
---
+++ Handler/PayslipShow.hs
@@ -0,0 +1,9 @@
+module Handler.PayslipShow where
+
+import Import
+
+getPayslipShowR :: Handler Html
+getPayslipShowR = error "Not yet implemented: getPayslipShowR"
+
+postPayslipShowR :: Handler Html
+postPayslipShowR = error "Not yet implemented: postPayslipShowR"
