34a4434a3980a113aa5cd7b5187d75f40e8bded4
Njagi Mwaniki <mwanikibusiness@gmail.com>
2015-02-18 14:33:00 +0300

    Updated PayslipShow to ensure a user is logged to run any of the handlers in it.

--- Handler/PayslipShow.hs
+++ Handler/PayslipShow.hs
@@ -4,13 +4,26 @@ import Import
 
 getPayslipShowR :: PayslipId ->  Handler Html
 getPayslipShowR payslipId = do
-  payslip <- runDB $ get404 payslipId
-  defaultLayout $ do
-    $(widgetFile "payslip/show")
+  maybeUser <- maybeAuth
+  case maybeUser of
+   Just (Entity uid _) -> do
+     payslip <- runDB $ get404 payslipId
+     defaultLayout $ do
+       $(widgetFile "payslip/show")
+   Nothing -> do
+     defaultLayout $ do
+       redirect $ HomeR
     
--- deletePayslipShowR :: moand m => PayslipId -> ReaderT (PersistEntityBackend Payslip) m ()
-deletePayslipShowR :: PayslipId -> Handler Html
+deletePayslipShowR :: PayslipId -> Handler ()
 deletePayslipShowR payslipId = do
-  runDB $ delete payslipId
-  defaultLayout $ do
-    $(widgetFile "payslip/deleted")
+  maybeUser <- maybeAuth 
+  maybePayslip <- runDB $ get payslipId
+  case maybeUser of
+   Just (Entity uid _) -> do
+     case maybePayslip of
+      Just payslip -> do
+        case ((payslipOwner payslip) == uid) of
+         True -> runDB $ delete payslipId
+         False -> return ()
+      Nothing -> return ()
+   Nothing -> return ()
