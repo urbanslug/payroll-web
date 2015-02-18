8ac1f4f9de33e93a00e863594c5da16b176e06b1
Njagi Mwaniki <mwanikibusiness@gmail.com>
2015-02-19 01:19:54 +0300

    Changed maybeAuth to requireAuth in Handler/PayslipShow.hs

--- Handler/PayslipShow.hs
+++ Handler/PayslipShow.hs
@@ -4,16 +4,11 @@ import Import
 
 getPayslipShowR :: PayslipId ->  Handler Html
 getPayslipShowR payslipId = do
-  maybeUser <- maybeAuth
-  case maybeUser of
-   Just (Entity uid _) -> do
-     payslip <- runDB $ get404 payslipId
-     defaultLayout $ do
+  (Entity uid _) <- requireAuth
+  payslip <- runDB $ get404 payslipId
+  defaultLayout $ do
        $(widgetFile "payslip/show")
-   Nothing -> do
-     defaultLayout $ do
-       redirect $ HomeR
-    
+{-
 deletePayslipShowR :: PayslipId -> Handler ()
 deletePayslipShowR payslipId = do
   maybeUser <- maybeAuth 
@@ -26,4 +21,15 @@ deletePayslipShowR payslipId = do
          True -> runDB $ delete payslipId
          False -> return ()
       Nothing -> return ()
+   Nothing -> return ()
+-}
+deletePayslipShowR :: PayslipId -> Handler ()
+deletePayslipShowR payslipId = do
+  (Entity uid _) <- requireAuth
+  maybePayslip <- runDB $ get payslipId
+  case maybePayslip of
+   Just payslip -> do
+     case ((payslipOwner payslip) == uid) of
+      True -> runDB $ delete payslipId
+      False -> return ()
    Nothing -> return ()
