a2c7f6783d63af3b73a37d3d169d9abea584b1f8
Njagi Mwaniki <mwanikibusiness@gmail.com>
2015-03-10 00:58:50 +0300

    Tests can sign in user but can't log them in.

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -2,68 +2,25 @@ module Handler.HomeSpec (spec) where
 
 import TestImport
 import Yesod.Auth(Route(LoginR))
+import TestTools
 
 spec :: Spec
 spec = withApp $ do
-  describe "Auth things" $ do
+    it "requires login" $ do
+      needsLogin GET ("/" :: Text)
+
     it "creates users" $ do
       get SignUpR
       statusIs 200
 
       request $ do
-        byLabel "username" "user"
-        byLabel "password" "p"
         setMethod "POST"
         setUrl SignUpR
-      statusIs 200
-      
-    it "logs users in" $ do
-      get (AuthR LoginR)
-      statusIs 200
-
-      bodyContains "Login"
-
-      request $ do
+        addToken
+        
         byLabel "username" "user"
         byLabel "password" "p"
-        setMethod "GET"
-        setUrl (AuthR LoginR)
-      statusIs 200
+      statusIs 303 -- redirects after successful signup
 
-    it "user has been logged in" $ do
-      get HomeR
-      statusIs 303
-      bodyContains "Login"
-    
-      
-
-{-
-spec :: Spec
-spec = withApp $ do
-    it "loads the index and checks it looks right" $ do
-        get HomeR
-        statusIs 200
-        htmlAllContain "h1" "Welcome to Yesod"
-
-        request $ do
-            setMethod "POST"
-            setUrl HomeR
-            addNonce
-            fileByLabel "Choose a file" "test/Spec.hs" "text/plain" -- talk about self-reference
-            byLabel "What's on the file?" "Some Content"
-
-        statusIs 200
-        -- more debugging printBody
-        htmlCount ".message" 1
-        htmlAllContain ".message" "Some Content"
-        htmlAllContain ".message" "text/plain"
-
-    -- This is a simple example of using a database access in a test.  The
-    -- test will succeed for a fresh scaffolded site with an empty database,
-    -- but will fail on an existing database with a non-empty user table.
-    it "leaves the user table empty" $ do
-        get HomeR
-        statusIs 200
-        users <- runDB $ selectList ([] :: [Filter User]) []
-        assertEqual "user table empty" 0 $ length users
--}
+    it "logs in a user" $ do
+      doLogin "user" "p"
